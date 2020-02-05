ea4641f6f3597d6f1d3353c869e721f09e6907cf
Camilo Moncada <monk-da@hotmail.com>
2020-02-05 16:34:26 -0500

    bugfix response question 4 hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,7 +9,7 @@
 int main(void)
 {
 char str1[55];
-strcpy(str1, "Programming is like building a multilingual puzzle");
+strcpy(str1, "\"Programming is like building a multilingual puzzle");
 puts(str1);
 return (0);
 }
