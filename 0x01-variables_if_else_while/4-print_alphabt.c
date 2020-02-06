841dbb337f7679ebae9046160aa45cff68741821
Camilo Moncada <monk-da@hotmail.com>
2020-02-06 15:52:07 -0500

    response question 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char n = 'a';
+while (n <= 'z')
+{
+  if (!(n == 'q' || n == 'e'))
+{
+putchar(n);
+}
+n++;
+}
+putchar('\n');
+return (0);
+}
