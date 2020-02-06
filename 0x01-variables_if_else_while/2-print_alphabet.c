44fbd511a0f5dcb5b528d877326e4826c90753a4
Camilo Moncada <monk-da@hotmail.com>
2020-02-06 15:32:40 -0500

    response question 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *main - Entry point
+ *
+ *Return Always 0
+ */
+
+int main(void)
+{
+char n = 'a';
+while(n <= 'z')
+{
+putchar(n);
+n++;  
+}
+putchar('\n');
+return (0);
+}
