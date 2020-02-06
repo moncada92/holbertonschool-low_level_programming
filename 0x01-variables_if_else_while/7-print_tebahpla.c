6e5def04c8fc4458023370e6c2c6bfbf97ec7ebd
Camilo Moncada <monk-da@hotmail.com>
2020-02-06 17:12:21 -0500

    response question 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char n = 'z';
+while (n >= 'a')
+{
+putchar(n);
+n--;
+}
+putchar('\n');
+return (0);
+}
