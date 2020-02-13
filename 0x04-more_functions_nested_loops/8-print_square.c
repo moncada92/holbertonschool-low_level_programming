878478bab3a69e43b298dc361a8dbe6595cdcccf
Camilo Moncada <monk-da@hotmail.com>
2020-02-13 15:35:34 -0500

    response question 8 prints square depending the size the numbers

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * print_square  - print the # in line .
+ *
+ * @n: varaible the parameter
+ * Return: 0
+ */
+void print_square(int n)
+{
+	int m, l;
+
+	l = n;
+
+	while (n > 0)
+	{
+		m = l;
+		while (m > 0)
+		{
+			_putchar('#');
+			m--;
+		}
+		n--;
+		_putchar('\n');
+	}
+}
