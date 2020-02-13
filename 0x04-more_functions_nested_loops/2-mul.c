c8e7913cd57d791ea65800542ff885b3c23ee23d
Camilo Moncada <monk-da@hotmail.com>
2020-02-13 12:58:00 -0500

    bugfix question 2 no write description the parameters

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,13 +3,12 @@
 /**
  * mul - check the is number 0 through 9.
  *
- * @c: is a variable or parameter
+ * @a: is a variable or parameter
+ * @b: is a variable or parameter
+ *
  * Return: 0 is true or 1 is false.
  */
 int mul(int a, int b)
 {
-	int result;
-
-	result = a * b;
-	return (result);
+	return (a * b);
 }
