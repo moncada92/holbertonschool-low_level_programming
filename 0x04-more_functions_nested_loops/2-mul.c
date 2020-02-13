e6a7a1521fddc9467d9e72d39386dbe6ffcaf797
Camilo Moncada <monk-da@hotmail.com>
2020-02-13 12:52:32 -0500

    response question 2 multiply two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * mul - check the is number 0 through 9.
+ *
+ * @c: is a variable or parameter
+ * Return: 0 is true or 1 is false.
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
