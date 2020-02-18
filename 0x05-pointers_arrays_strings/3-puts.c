bafba8eedd385ffaa85fdd208f6799e87aed2c2d
Camilo Moncada <monk-da@hotmail.com>
2020-02-18 17:26:02 -0500

    response question 3 print array pointer

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _puts - print a string parameter
+ *
+ * @str:this is a pointer string
+ *
+ * Return: Always value void.
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for(i = 0; *(str + i) != '\0'; i++)
+	{
+		_putchar(*(str + i));
+	}
+	_putchar('\n');
+}
