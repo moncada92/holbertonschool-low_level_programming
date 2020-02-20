0f82101e78b7d9d1ebe72370f2bedf02c6a73821
Camilo Moncada <monk-da@hotmail.com>
2020-02-20 15:25:10 -0500

    bug fix berror code 2 response question 2 copy string pointer

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,13 +14,15 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
-		
-	while (n > 0)
+
+	while (i < n && src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
-		n--;
 	}
-	
+	for(; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
 	return (dest);
 }
