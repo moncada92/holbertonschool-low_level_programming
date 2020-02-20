9c675787ebc0c20598bbd280f0bfbddc71302344
Camilo Moncada <monk-da@hotmail.com>
2020-02-20 14:24:23 -0500

    response question 1 copy and push n characteres in a array

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ * _strncat - copy string and append ohter string in n .
+ *
+ * @dest: parameter type pointer
+ *
+ * @src: parameter type pointer
+ *
+ * @n: parameter type int
+ *
+ * Return: dest pointer
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j, k = 0;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	for (j = 0; src[j] != '\0'; j++)
+		;
+	while (k <= n)
+	{
+		if (n > 0)
+		{
+			dest[i] = src[k];
+			i++;
+			k++;
+			n--;
+		}
+		else
+		{
+			break;
+		}
+	}
+	return (dest);
+}
