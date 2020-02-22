4ec04de8ecfd2e754241fc44eb9ab9c67a3d788c
Camilo Moncada <monk-da@hotmail.com>
2020-02-21 22:49:54 -0500

    response question 8 bugfix3 space in first line, create code rot13

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,24 +1,27 @@
 #include "holberton.h"
 
 /**
- * rot13 - rotate characters 13 places in the alphabet
- * @s: string
- * Return: string `s` rotated
+ * rot13 - Function that reverses the content of an array of integers.
+ *
+ * @str: s is the array
+ *
+ * Return: Always 0.
  */
-
-char *rot13(char *s)
+char *rot13(char *str)
 {
-	int i;
-	char storeh[] = "NOPQRSTUVWXYZABCDEFGHIJKLM";
-	char storel[] = "nopqrstuvwxyzabcdefghijklm";
+	int i = 0, k;
+	char s[] =  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char s1[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (; str[i] != '\0'; i++)
 	{
-		if ((s[i] > 64 && s[i] < 91) || (s[i] > 96 && s[i] < 123))
+		for (k = 0; k <= 51; k++)
 		{
-			s[i] = (s[i] - 65 > 25) ?
-				storel[s[i] - 97] : storeh[s[i] - 65];
+			if (s[k] == str[i])
+			{
+				str[i] = s1[k];
+			}
 		}
 	}
-	return (s);
+	return (str);
 }
