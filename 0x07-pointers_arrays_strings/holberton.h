07f6ab9159f8d46236d3d19522fbb4670d8ea80c
Camilo Moncada <monk-da@hotmail.com>
2020-02-25 23:46:07 -0500

    response question 5

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -5,3 +5,4 @@ unsigned int _strspn(char *s, char *accept);
 int _putchar(char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
