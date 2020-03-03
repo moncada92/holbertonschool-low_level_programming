aa8e45c73f83ccb2db6e2014e08c2a51709ff167
Camilo Moncada <monk-da@hotmail.com>
2020-03-03 13:44:23 -0500

    bugfix error pointer  question 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -22,6 +22,11 @@ char *create_array(unsigned int size, char c)
 	{
 		char *p = (char *)malloc(size * sizeof(char));
 
+		if (p == 0)
+		{
+			return (0);
+		}
+
 		for (i = 0; i < size; i++)
 		{
 			p[i] = c;
