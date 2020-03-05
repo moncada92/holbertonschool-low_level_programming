a2e126328d92f728efc80a3decb10b859ae42019
Camilo Moncada <monk-da@hotmail.com>
2020-03-05 14:59:28 -0500

    response question 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * *_calloc - create funcion caloc
+ * @nmemb: characteres a copy
+ * @size: characteres copy with n
+ * Return: void
+ **/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int k;
+	char *dest;
+
+	if (nmemb == 0)
+	{
+		return (0);
+	}
+	if (size == 0)
+	{
+		return (0);
+	}
+
+	dest = (char *)malloc(nmemb * size);
+	if (dest == 0)
+	{
+		return (0);
+	}
+	for (k = 0; k < size; k++)
+	{
+		*(dest + k)  = 0;
+	}
+	return (dest);
+}
