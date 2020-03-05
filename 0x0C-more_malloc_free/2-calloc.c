02ce11f321ac104dfdb7aef98bae859d03df5f4d
Camilo Moncada <monk-da@hotmail.com>
2020-03-05 16:58:38 -0500

    bugfix 2 question 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,26 +14,18 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int k;
 	char *dest;
 
-	if (nmemb == 0)
+	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
-	if (size == 0)
-	{
-		return (NULL);
-	}
-
-	dest = (char *)malloc(nmemb * size);
-	if (dest == 0)
+	dest = malloc(nmemb * size);
+	if (dest == NULL)
 	{
 		return (NULL);
 	}
 	for (k = 0; k < size; k++)
 	{
-		if (dest[k]  != 0)
-		{
-			dest[k] = 0;
-		}
+		*(dest + k) = '0';
 	}
 	return (dest);
 }
