5ad6c130c7d5ca55ba5d97d1ebf09fe3a66d3421
Camilo Moncada <monk-da@hotmail.com>
2020-03-09 16:37:40 -0500

    response question 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+/**
+ * init_dog - create structure dog.
+ * @d: First member
+ * @name: Second member
+ * @age: Third member
+ * @owner: Fourth member
+ * Return: notghin is void
+ **/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+
+}
