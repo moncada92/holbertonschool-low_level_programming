9db220f77e76062aa025c14c9b236e87805ff176
Camilo Moncada <monk-da@hotmail.com>
2020-03-09 12:29:08 -0500

    response question 0 at 4 the preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - funcion print the name file
+ * Return: zero because is only print string
+ **/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+<h1> Learn about Preprocessor - Macros in C </h1>
