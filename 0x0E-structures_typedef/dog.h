0729acef5f2283f08bec8e13ad49bcfff9db2f7f
Camilo Moncada <monk-da@hotmail.com>
2020-03-09 16:20:12 -0500

    bugfix question 0, forget add #define

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,15 +1,20 @@
-int _putchar(char c);
+#ifndef DOG_H
+
+#define DOG_H
+
 /**
- * struct my_struct - Short description
+ * struct dog - Structure data dog
  * @name: First member
  * @age: Second member
  * @owner: Third member
- *
- * Description: Longer description
  */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+
+#endif
