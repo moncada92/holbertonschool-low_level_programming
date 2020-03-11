90db23b77d668f9c5f2506e939f08d610c250737
Camilo Moncada <monk-da@hotmail.com>
2020-03-11 15:52:13 -0500

    bugfix question 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,20 +13,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size >  0 || array != NULL || cmp != NULL)
-	{
-		for (i = 0; i < size ; i++)
-		{
-			if (cmp(array[i]))
-			{
-				return (i);
-			}
-		}
-
-	}
-	else
-	{
+	if (size <=  0 || array == NULL || cmp == NULL)
 		return (-1);
+
+	for (i = 0; i < size ; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
 	}
 
 	return (-1);
