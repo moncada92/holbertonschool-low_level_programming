8f74bfc4b1ffb6e0a6c85a3c9e8a7a23252c8bdb
Camilo Moncada <monk-da@hotmail.com>
2020-03-12 06:39:59 -0500

    bugfix4 question 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,63 +1,33 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "3-calc.h"
-
+#include"3-calc.h"
 /**
- * main - check the code for Holberton School students.
- *
- * @argc: number the arguments
- * @argv: array thw argumnets
- * Return: Always 0.
- */
+ * get_op_func - get the operator and redirect to the func.
+ * @s: the operator
+(* a blank line
+* Description: get the operator and redirect to the func.)?
+(* section header: 3-calc.h)*
+* Return: return null or the function to do.
+*/
 
-int main(int argc, char *argv[])
-{
+	 int (*get_op_func(char *s))(int, int)
+	 {
+		 op_t ops[] = {
+			 {"+", op_add},
+			 {"-", op_sub},
+			 {"*", op_mul},
+			 {"/", op_div},
+			 {"%", op_mod},
+			 {NULL, NULL}
+		 };
+		 int i;
 
-	printf("argc: %d\n", argc);
-	int num1, num2, operator, result;
-
-	int (*p)(int, int);
-
-	if (argc != 4)
-	{
-		printf("ERROR!!!\n");
-		exit(98);
-
-	}
-
-	if (strlen(argv[2]) != 1)
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
-
-	p = get_op_func(argv[2]);
-
-	operator = argv[2][0];
-
-	switch (operator)
-	{
-	case '+':
-		break;
-	case '-':
-		break;
-	case '*':
-		break;
-	case '/':
-		break;
-	case '%':
-		break;
-	default:
-		printf("Error\n");
-		exit(99);
-	}
-
-	result = p(num1, num2);
-
-	printf("%d\n", result);
-
-	return (0);
-}
+		 i = 0;
+		 while (i < 5)
+		 {
+			 if (strcmp(s, ops[i].op) == 0)
+			 {
+				 return (*(ops[i]).f);
+			 }
+			 i++;
+		 }
+		 return (NULL);
+	 }
