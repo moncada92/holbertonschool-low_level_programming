8ef24f5e14255074278a8232bc13b111035df66e
Camilo Moncada <monk-da@hotmail.com>
2020-03-15 23:44:50 -0500

    bugfix question 4 advanced

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,23 +1,23 @@
-;  /usr/local/bin/nasm -f macho64 64.asm && ld -macosx_version_min 10.7.0 -lSystem -o 64 64.o && ./64
+%define sys_write 1
+	%define stdout 1
 
-	global start
+	%define sys_exit 60
+	%define success 0
 
-
-	section .text
-
-start:
-	    mov     rax, 0x2000004 ; write
-	    mov     rdi, 1	   ; stdout
-	    mov     rsi, msg
-	    mov     rdx, msg.len
-	    syscall
-
-	    mov     rax, 0x2000001 ; exit
-	    mov     rdi, 0
-	    syscall
-
+	%define nl 10
 
 	section .data
 
-msg:	    db      "Hello, world!", 10
-	.len:   equ     $ - msg
+	message db "Hello, Holberton", nl
+
+	section .text
+	global main
+main:
+	mov rax, sys_write
+	mov rdi, stdout
+	mov rsi, message
+	mov rdx, 17
+	syscall
+	mov    rax, sys_exit
+	mov    rdi, success
+	syscall
