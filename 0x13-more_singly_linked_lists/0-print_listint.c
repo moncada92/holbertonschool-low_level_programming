21e473e384c2f8138b39f24b98890341ec9ea349
Camilo Moncada <monk-da@hotmail.com>
2020-03-24 14:54:22 -0500

    response question 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_listint - print elemets
+ * @h: list linked
+ * Return: size string
+ **/
+
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	if (h != NULL)
+	{
+		for (; h; count++)
+		{
+			printf("%d \n", h->n);
+			h = h->next;
+		}
+		return (count);
+	}
+
+	return (0);
+}
