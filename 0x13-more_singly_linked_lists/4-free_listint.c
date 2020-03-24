ef7f42347bb12b5ca4a862f06affd4781324f806
Camilo Moncada <monk-da@hotmail.com>
2020-03-24 17:29:44 -0500

    response question 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * free_listint - free list
+ * @head: list linked
+ * Return: nothing
+ **/
+
+void free_listint(listint_t *head)
+{
+	listint_t *pos;
+
+	if (head != NULL)
+	{
+		while(head != NULL)
+		{
+			pos = head;
+			head = head->next;
+			free(pos);
+		}
+
+	}
+	else
+	{
+		return;
+	}
+}
