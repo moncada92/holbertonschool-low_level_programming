ed5920653d3f174c9d81eedf261bda88af195ec8
Camilo Moncada <monk-da@hotmail.com>
2020-05-02 17:12:47 -0500

    response question 1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - function that prints all the elements of a dlistint_t list.
+ * @h: input of a node.
+ * Return: Always EXIT_SUCCESS.
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *tmp = h;
+	size_t i = 0;
+
+	while (tmp)
+	{
+		tmp = tmp->next;
+		i++;
+	}
+	return (i);
+}
