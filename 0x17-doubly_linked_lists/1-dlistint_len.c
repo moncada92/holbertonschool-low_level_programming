29071075bc88385c5b834aea16a9eca05b885bbd
Camilo Moncada <monk-da@hotmail.com>
2020-05-02 17:14:17 -0500

    bugfix question 1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 
 /**
- * print_dlistint - function that prints all the elements of a dlistint_t list.
+ * dlistint_len - function that prints all the elements of a dlistint_t list.
  * @h: input of a node.
  * Return: Always EXIT_SUCCESS.
  */
 
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	const dlistint_t *tmp = h;
 	size_t i = 0;
