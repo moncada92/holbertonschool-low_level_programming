8fac10f9d1b0110ffe197308126c48dfe9dc72d8
Camilo Moncada <monk-da@hotmail.com>
2020-05-02 17:17:59 -0500

    response question 2

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - print the elements in a doble linked list.
+ *
+ * @h: list
+ * Description: print the elements of one list)?
+ * section header: the header of this function is lists.h
+ * Return: this return the num of the elements in the list
+ **/
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new = NULL;
+
+	new = malloc(sizeof(dlistint_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = *head;
+	new->prev = NULL;
+	
+	if(*head != NULL)
+	{
+		(*head)->prev = new;
+	}
+	*head = new
+
+	return (new);
+}
