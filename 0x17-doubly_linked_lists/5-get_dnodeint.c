dd3063a18519663c872b1b7afe369937fa54e79e
Camilo Moncada <monk-da@hotmail.com>
2020-05-03 12:01:17 -0500

    response question 5

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - add node head double linked list.
+ *
+ * @head: list
+ * @index: index the node
+ *
+ * Return: this return the num of the elements in the list
+ **/
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *new = NULL;
+	dlistint_t *tmp = head;
+	unsigned int i = 0;
+
+	new = malloc(sizeof(dlistint_t));
+
+	if (new == NULL)
+		return (NULL);
+
+
+	while (i < index)
+	{
+		if (tmp->next == NULL)
+			return (NULL);
+		tmp = tmp->next;
+		i++;
+	}
+		new->n = tmp->n;
+
+	return (new);
+}
