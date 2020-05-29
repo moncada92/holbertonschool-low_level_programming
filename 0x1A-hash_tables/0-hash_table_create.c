84bd61a5a3dac6376e313e3b802bc6ecd913a1d6
Camilo Moncada <monk-da@hotmail.com>
2020-05-29 15:01:46 -0500

    response question 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - function for create hash table
+ * @size: size max the table
+ * Return: a hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int i = 0;
+
+	/*allocate table */
+	hash_table_t *t_hsh = malloc(sizeof(hash_table_t) * 1);
+
+	if (t_hsh == NULL)
+		return (NULL);
+
+	/*allocate table array*/
+	t_hsh->array = malloc(sizeof(hash_node_t *) * size);
+
+	if (t_hsh->array == NULL)
+		return (NULL);
+
+	/*set each to null */
+	for (; i < size; i++)
+	{
+		t_hsh->array[i] = NULL;
+	}
+
+	return (t_hsh);
+}
