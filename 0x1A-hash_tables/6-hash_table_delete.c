ba3e6c9a271c3f697180e3e12c2c6dcf90dcf1fc
Camilo Moncada <monk-da@hotmail.com>
2020-05-29 23:51:55 -0500

    hotfix 2 question 6

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,45 +1,34 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_delete - delete the hash table
- * @ht: Struct Hash Table
- * Return: None
+ * hash_table_delete - delete a table
+ * @ht: hash
+ * Return: void no return
  */
-
 void hash_table_delete(hash_table_t *ht)
 {
-	unsigned int index;
-	hash_node_t *tmp;
 
-	while (index < ht->size)
+	hash_node_t *actual, *aux;
+	unsigned int i = 0;
+
+	if (ht == NULL)
+		return;
+	for (i = 0; i < ht->size; i++)
 	{
-		if (ht->array[index] != NULL)
+		if (ht->array[i] != NULL)
 		{
-			tmp = ht->array[index];
-			free_list(tmp);
-			ht->array[index] = NULL;
+			actual = ht->array[i];
+			while (actual != NULL)
+			{
+				aux = actual;
+				free(actual->key);
+				if (actual->value != NULL)
+					free(actual->value);
+				actual = actual->next;
+				free(aux);
+			}
 		}
-		index++;
 	}
 	free(ht->array);
 	free(ht);
-	ht = NULL;
-}
-
-/**
- * free_list - Realease the memory allocated for a list
- *
- * @head: A pointer to the first node of the list to free
- */
-
-void free_list(hash_node_t *head)
-{
-	if (head)
-	{
-		free_list(head->next);
-		free(head->key);
-		free(head->value);
-		free(head);
-
-	}
 }
