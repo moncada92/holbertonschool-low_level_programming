2a442c8d627c20fc351335157d6d8af701765c0c
Camilo Moncada <monk-da@hotmail.com>
2020-05-29 23:41:43 -0500

    hotfix question 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,78 +1,53 @@
 #include "hash_tables.h"
 
 /**
- * ht_rep - add node in the entry in the hash table
- * @value: value the date
- * @key: key de the value
- * Return: a hash_node_t value
- */
-
-hash_node_t *ht_rep(const char *key, const char *value)
-{
-	hash_node_t *node = malloc(sizeof(node) * 1);
-
-	node->key = malloc(strlen(key) + 1);
-	node->value = malloc(strlen(value) + 1);
-
-	if (node == NULL || node->key == NULL || node->value == NULL)
-		return (NULL);
-
-	strcpy(node->key, key);
-	strcpy(node->value, value);
-
-	node->next = NULL;
-
-	return (node);
-}
-
-
-/**
- * hash_table_set - insert nodes in the hash table
- * @value: value the node
- * @key:  key the data info
- * @ht: hash table created
- * Return: 1 is success or 0 it is error
+ * hash_table_set - function put the key and value in the index
+ * @ht: Struct Hash Table
+ * @value: value of the key
+ * @key: is a data or name.
+ * Return: the new hash table
  */
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	/* get hash_key (position) in the hash table  */
-	unsigned long int index = (key_index((unsigned char *)key, ht->size));
 
-	if (key == NULL)
+	unsigned int idx;
+	hash_node_t *node;
+
+	if (ht == NULL)
 		return (0);
-
-	/*asignament position for the value in hash table */
-	hash_node_t *entry = ht->array[index];
-
-	/*add dates in a linked list in the entry the hash table*/
-	if (entry == NULL)
+	if (key == NULL || *key == '\0')
+		return (0);
+	idx = key_index((unsigned char *)key, ht->size);
+	if (ht->array[idx] != NULL)
 	{
-		ht->array[index] = ht_rep(key, value);
-
-		if (ht->array[index] == NULL)
-			return (1);
-
-		return (1);
-	}
-	hash_node_t *tmp;
-	/* is a collision*/
-	while (entry != NULL)
-	{
-		if (strcmp(entry->key, key) == 0)
+		if (strcmp(key, ht->array[idx]->key) == 0)
 		{
-			/*match found, replace value*/
-			free(entry->value);
-			entry->value = malloc(strlen(value) + 1);
-			strcpy(entry->value, value);
+			ht->array[idx]->value = strdup(value);
 			return (1);
 		}
-
-		/*walk to next*/
-		tmp = entry;
-		entry = tmp->next;
 	}
-	/*if not match add new a node*/
-	tmp->next = ht_rep(key, value);
-	return (0);
+
+	if (ht->array[idx] == NULL)
+	{
+		node = malloc(sizeof(hash_node_t *));
+		if (node == NULL)
+			return (0);
+		node->next = NULL;
+		node->key = strdup(key);
+		node->value = strdup(value);
+		ht->array[idx] = node;
+	}
+	else
+	{
+		node = malloc(sizeof(hash_node_t *));
+		if (node == NULL)
+			return (0);
+		node->key = strdup(key);
+		node->value = strdup(value);
+		node->next = ht->array[idx];
+		ht->array[idx]  = node;
+	}
+
+	return (1);
 }
