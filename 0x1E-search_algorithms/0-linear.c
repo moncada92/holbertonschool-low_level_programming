062edd2a4a5b5ceb22b0219103315f39ba6722bf
Camilo Moncada <monk-da@hotmail.com>
2020-08-26 12:18:46 -0500

    hotfix 3 question 0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -18,7 +18,7 @@ int linear_search(int *array, size_t size, int value)
 		result = -1;
 	}
 
-	while (i < (int)size_a)
+	while (i < (int)size)
 	{
 		printf("Value cheked array[%d] = [%d]\n", i, array[i]);
 		if (array[i] == value)
